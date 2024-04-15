55a4c58e16060d16a469157a15f6f02bf632dcbe
feliposz <feliposz@gmail.com>
2024-04-14 21:57:03 -0300

    Respond with 200

--- app/main.js
+++ app/main.js
@@ -8,6 +8,8 @@ const server = net.createServer((socket) => {
     socket.end();
     server.close();
   });
+  socket.write("HTTP/1.1 200 OK\r\n\r\n");
+  socket.end();
 });
 
 server.listen(port, hostname);
