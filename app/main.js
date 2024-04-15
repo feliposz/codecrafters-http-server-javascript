3438bd02b29579c260a38e549d0e55952819e34c
feliposz <feliposz@gmail.com>
2024-04-14 22:05:52 -0300

    Respond with 404

--- app/main.js
+++ app/main.js
@@ -4,12 +4,21 @@ const hostname = "0.0.0.0";
 const port = 4221;
 
 const server = net.createServer((socket) => {
-  socket.on("close", () => {
-    socket.end();
-    server.close();
-  });
-  socket.write("HTTP/1.1 200 OK\r\n\r\n");
-  socket.end();
+    socket.on("close", () => {
+        socket.end();
+        server.close();
+    });
+    socket.on("data", (data) => {
+        const request = data.toString();
+        const lines = request.split("\r\n");
+        const [method, path, version] = lines[0].split(" ")
+        if (path === "/") {
+            socket.write("HTTP/1.1 200 OK\r\n\r\n");
+        } else {
+            socket.write("HTTP/1.1 404 Not found\r\n\r\n");
+        }
+        socket.end();
+    });
 });
 
 server.listen(port, hostname);
